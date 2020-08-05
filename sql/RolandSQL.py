35453bb9e7197ba55e47bd209ff770ceff6e4243
SKullDugDev <69214390+SKullDugDev@users.noreply.github.com>
2020-08-04 22:15:05 -0400

    Update RolandSQL.py

--- sql/RolandSQL.py
+++ sql/RolandSQL.py
@@ -12,68 +12,96 @@ class SQLRunner:
     # connecting to Roland
 
     def __init__(self):
-        pass
-
-    def add_new_campgn_nm(self, usr_campgn_nm):
-        cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
+        self.cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
                                    'Server=MJÖLNIR;'
                                    'Database=Roland;'
                                    'Trusted_Connection=yes;')
-        cursor = cnxn.cursor()
-        self.old_campgn_nm_pull(usr_campgn_nm)
-        self.campgn_list = old_campgn_nm_pull.campgn_list
-        self.chk_failed = add_new_campgn_chk(campgn_list, usr_campgn_nm)
-        return self.chk_failed
+        self.cursor = self.cnxn.cursor()
+        self.result = None
+        self.campaign_list = None
+        self.check_failed = None
 
-    def old_campgn_nm_pull(self, usr_campgn_nm):
-        # check db for CampaignName; store in rchable var
-        cnxn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                                   'Server=MJÖLNIR;'
-                                   'Database=Roland;'
-                                   'Trusted_Connection=yes;')
-        cursor = cnxn.cursor()
-        result = cursor.execute(sql_chk_campgn_nm, usr_campgn_nm)
-        sqlog.logger.info("%s\n Parameters: %s", sql_chk_campgn_nm, usr_campgn_nm)
-        self.old_campgn_nm_pull.campgn_list = result.fetchall()
-        return self.old_campgn_nm_pull.campgn_list
+    def close(self):
+        # close other cursors and then close the connection
 
-    def add_new_campgn_chk(self, campgn_list, usr_campgn_nm):
-        output = True
+        cursor = self.cnxn.cursor()
+        self.cursor.close()
+        self.cnxn.close()
+
+    def process_to_add_new_campaign_name(self, user_campaign_name):
+        # begins campaign name adding and rejecting logic
+
+        cursor = self.cnxn.cursor()
+
+        # check if the campaign name is in the db and store the results in the campaign list variable
+
+        self.campaign_list = self.check_if_campaign_name_is_in_database(user_campaign_name)
+        self.check_if_campaign_name_is_new(self.campaign_list, user_campaign_name)
+        return
+
+    def check_if_campaign_name_is_in_database(self, user_campaign_name):
+        # check database for campaign name and return the result
+
+        cursor = self.cnxn.cursor()
+        self.result = self.cursor.execute(sql_check_if_campaign_name_in_database, user_campaign_name)
+        sqlog.logger.info("%s\n Parameters: %s", sql_check_if_campaign_name_in_database, user_campaign_name)
+        campaign_list = self.result.fetchall()
+        return campaign_list
+
+    def check_if_campaign_name_is_new(self, campgn_list, user_campaign_name):
+        # if it failed the check it will automatically close the transaction and log it
+
+        self.check_failed = True
+
+        # if the name wasn't found in the database that means it is new and things can move on
+
         if not campgn_list:
-            mk_new_campgnid()
-            new_campgnid = mk_new_campgnid.new_campgnid
-            sqladd_new_campgn_nm(usr_campgn_nm, new_campgnid)
-            output = False
+            # get the new campaign id and store it
+
+            new_campaign_id = self.make_new_campaign_id()
+
+            # begin progress to add the new campaign name to the database
+
+            self.add_new_campaign_name_to_database(user_campaign_name, new_campaign_id)
+
+            # set the check as not having failed
+
+            self.check_failed = False
+            sqlog.logger.info('Passed Check...adding name...')
         else:
             sqlog.logger.info('Transaction Closed. Name already exists')
-        return output
+        return self.check_failed
 
-    def mk_new_campgnid(self):
-        # get max CampaignId, +1, store in rchable var
-        cursor.execute(sql_max_campgnid)
-        current_max_campgnid = cursor.fetchone()[0]
-        mk_new_campgnid.new_campgnid = current_max_campgnid + 1
+    def make_new_campaign_id(self):
+        # get max CampaignId, +1, store and return
+
+        cursor = self.cnxn.cursor()
+        cursor.execute(sql_get_max_campaign_id)
+        current_max_campaign_id = cursor.fetchone()[0]
+        new_campaign_id = current_max_campaign_id + 1
+        return new_campaign_id
+
+    def add_new_campaign_name_to_database(self, user_campaign_name, new_campaign_id):
+        # add new campaign name
+        cursor = self.cnxn.cursor()
+        cursor.execute(sql_insert_new_campaign_name, new_campaign_id, user_campaign_name)
+        sqlog.logger.info("%s\n Parameters: %s, %s", sql_insert_new_campaign_name, new_campaign_id, user_campaign_name)
         return
 
-    def sqladd_new_campgn_nm(self, usr_campgn_nm, new_campgnid):
-        # add new CampaignName
-        result = cursor.execute(sql_new_usr_campgn, new_campgnid, usr_campgn_nm)
-        sqlog.logger.info("%s\n Parameters: %s, %s", sql_new_usr_campgn, new_campgnid, usr_campgn_nm)
-        return
-
-    def chk_new_campgn_add(self, usr_campgn_nm):
+    def check_campaign_name_before_committing(self, user_campaign_name):
         # find new campaign
-        result = cursor.execute(sql_chk_campgn_nm, usr_campgn_nm)
+        cursor = self.cnxn.cursor()
+        cursor.execute(sql_check_if_campaign_name_in_database, user_campaign_name)
         check_new_campgn_jrney = cursor.fetchone()
         if check_new_campgn_jrney:
             try:
-                cnxn.commit()
+                self.cnxn.commit()
             except Exception as e:
                 logging.exception("Commit Error Occurred")
         else:
             logging.info('New campaign journey failed. Lost along the way.')
             return
 
-test_run = SQLRunner()
-print(test_run.add_new_campgn_nm('TheShrub'))
-test_run.close()
+db = SQLRunner()
+db.process_to_add_new_campaign_name('Frankenshrub')
+db.close()
