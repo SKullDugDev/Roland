75d426e2d6e44e81e950465f6e3b0095c30da839
SKullDugDev <69214390+SKullDugDev@users.noreply.github.com>
2020-08-12 09:27:25 -0400

    Update RolandSQL.py

--- sql/RolandSQL.py
+++ sql/RolandSQL.py
@@ -5,10 +5,12 @@ import pathlib
 import toml
 
 # for class variable type hints
-from typing import ClassVar, TypedDict, List
+from typing import ClassVar, TypedDict, List, Set
 
 # for connection
+import pyodbc
 import SQLRunner
+import itertools
 
 # for logging
 import sqlog
@@ -30,7 +32,6 @@ database = SQLRunner.SQLRunner()
 class CampaignInfo:
     # get sql variables
 
-    CHECK_IF_CAMPAIGN_NAME_IN_DATABASE: ClassVar[str] = sql_variables['CHECK_IF_CAMPAIGN_NAME_IN_DATABASE']
     INSERT_NEW_CAMPAIGN_NAME: ClassVar[str] = sql_variables['INSERT_NEW_CAMPAIGN_NAME']
     GET_CAMPAIGN_LIST: ClassVar[str] = sql_variables['GET_CAMPAIGN_LIST']
     DELETE_CAMPAIGN_BY_NAME: ClassVar[str] = sql_variables['DELETE_CAMPAIGN_BY_NAME']
@@ -42,59 +43,58 @@ class CampaignInfo:
 
         self.campaigns: List[str] = []
         self.check_failed: bool = True
-        self.invalid_campaigns_not_to_add: set = set()
-        self.valid_campaigns_to_add: set = set()
-        self.valid_campaign_for_removal: set = set()
 
-    def process_to_add_new_campaigns(self, new_campaigns_from_user, server_name: str) -> bool:
-        # add or reject new campaigns
+    def add_campaign_validation(self, campaigns_adding, server_name: str) -> str:
+        # for every campaign submitted, take the caseless version and put it in a set
+        sqlog.logger.info('Setting up new campaigns...')
+        campaigns_adding = {name.casefold() for name in campaigns_adding}
+        # for every current campaign, take the caseless version and put it in a set
+        sqlog.logger.info('Getting current campaigns...')
+        current_campaigns = {name.casefold() for name in self.get_campaigns(server_name)}
+        # campaigns already in the database shouldn't be added; this intersection reveals that set
+        sqlog.logger.info('Validating...')
+        invalid_campaigns = current_campaigns.intersection(campaigns_adding)
+        # from the set campaign set we want to add, remove what we have already, title them and make a new set
+        valid_campaigns = {name.title() for name in
+                           campaigns_adding.difference(invalid_campaigns)}
+        # gonna try something here to make a string and return that instead
+        return self.add_campaign_process(valid_campaigns, server_name, invalid_campaigns)
 
-        # sets campaigns to be added, literally
-        campaigns_to_be_added: set = set(new_campaigns_from_user)
+    def add_campaign_process(self, valid_campaigns, server_name, invalid_campaigns):
+        # if there are no valid campaigns, end and return a failure message
+        if not valid_campaigns:
+            return f"Already exists on {server_name}"
+        # else, add the campaigns in
+        sqlog.logger.info('Adding new campaigns...')
+        self.add_new_campaigns(valid_campaigns, server_name)
+        sqlog.logger.info('Campaigns successfully added...Checking commit requirements...')
+        # check they were added before committing
+        return self.add_campaign_commit_check(set(self.get_campaigns(server_name)), valid_campaigns, invalid_campaigns)
 
-        # get campaign list set
-        current_campaigns: set = set(self.get_campaigns(server_name))
-
-        # campaigns not to add
-        self.invalid_campaigns_not_to_add = current_campaigns.intersection(campaigns_to_be_added)
-
-        # campaigns to add
-        self.valid_campaigns_to_add = campaigns_to_be_added.difference(self.invalid_campaigns_not_to_add)
-
-        # add campaigns to database, return status
-        self.check_failed = self.add_campaigns_to_database(self.valid_campaigns_to_add, server_name)
-
-        return self.check_failed
-
-    def add_campaigns_to_database(self, valid_campaigns_to_add, server_name):
-        if valid_campaigns_to_add:
-            # if there are valid campaigns to add
-            sqlog.logger.info('Adding new campaigns...')
-            # add new campaigns
-            self.add_new_campaigns(valid_campaigns_to_add, server_name)
-            sqlog.logger.info('Campaigns successfully added...Checking commit requirements...')
-            current_campaigns = set(self.get_campaigns(server_name))
-            # check they were added before committing
-            self.add_campaign_commit_check(current_campaigns, valid_campaigns_to_add)
-        else:
-            # all the new names failed the check
-            self.check_failed = True
-        return self.check_failed
-
-    def add_new_campaigns(self, valid_campaigns_to_add, server_name):
+    def add_new_campaigns(self, campaigns_adding, server_name):
         # add each campaign that is listed as valid
-        for valid_campaigns in valid_campaigns_to_add:
-            database.cursor.execute(self.INSERT_NEW_CAMPAIGN_NAME, valid_campaigns, server_name)
+        try:
+            param = zip(itertools.repeat(server_name), campaigns_adding)
+            database.cursor.executemany(self.INSERT_NEW_CAMPAIGN_NAME, param)
             sqlog.logger.info("%s\n Parameters: %s, %s", self.INSERT_NEW_CAMPAIGN_NAME,
-                              valid_campaigns, server_name)
+                              campaigns_adding, server_name)
+        except pyodbc.DatabaseError:
+            sqlog.logger.exception('Error Exception Occurred')
 
-    def add_campaign_commit_check(self, current_campaigns, valid_campaigns_to_add):
-        if current_campaigns.intersection(valid_campaigns_to_add):
-            # if valid campaigns are now in database, commit
-            sqlog.logger.info('Commit requirements passed..committing...')
-            database.finalize()
-            sqlog.logger.info('Committed...')
-            self.check_failed = False
+    def add_campaign_commit_check(self, current_campaigns, valid_campaigns, invalid_campaigns):
+        if not current_campaigns.intersection(valid_campaigns):
+            # if the set is empty, they weren't added so fail
+            return 'Campaigns not added. Talk to Ra.'
+        # else
+        sqlog.logger.info('Commit requirements passed..committing...')
+        database.finalize()
+        sqlog.logger.info('Committed...')
+        # by this point, we know there is a valid campaign so if there are no invalids, the message is just what's added
+        if not invalid_campaigns:
+            return f"{', '.join([str(s) for s in valid_campaigns])} added"
+        # if there is an invalid, mention it wasn't added
+        return f"{', '.join([str(s) for s in valid_campaigns])} added;" \
+               f" {', '.join([str(s) for s in invalid_campaigns])} already exist "
 
     def get_campaigns(self, server_name):
         # get campaigns
@@ -107,67 +107,52 @@ class CampaignInfo:
         campaign_list = [row.CampaignName for row in query]
         return campaign_list
 
-    def delete_campaign_by_name(self, campaigns_set_for_removal: tuple, server_name: str) -> str:
-        # set campaigns to be removed, literally
-        campaigns_set_for_removal: set = set(campaigns_set_for_removal)
-        # get the set of current campaigns
-        current_campaigns: set = set(self.get_campaigns(server_name))
+    def remove_campaigns_process(self, campaigns_removing, server_name: str) -> str:
+        # for every campaign submitted for removal, take the caseless version and put it in a set
+        sqlog.logger.info('Setting up campaigns for removal...')
+        campaigns_removing = {name.casefold() for name in campaigns_removing}
+        # get the caseless set of current campaigns
+        sqlog.logger.info('Getting current campaigns...')
+        current_campaigns = {name.casefold() for name in self.get_campaigns(server_name)}
         # find which campaigns are in the current list, which thus can be removed
-        self.valid_campaign_for_removal = campaigns_set_for_removal.intersection(current_campaigns)
+        sqlog.logger.info('Validating...')
+        valid_campaigns = campaigns_removing.intersection(current_campaigns)
         # invalid campaigns will not be removed; is not a campaign
-        invalid_campaign_for_removal = campaigns_set_for_removal.difference(self.valid_campaign_for_removal)
+        invalid_campaigns = campaigns_removing.difference(valid_campaigns)
         sqlog.logger.info('Deleting campaigns...')
         # gets a set of the campaigns being removed
-        campaigns_being_removed_set: set = self.individual_campaign_deletion(self.valid_campaign_for_removal,
-                                                                             server_name)
-        # check if the campaigns are really deleted and commit
-        self.are_campaigns_deleted_properly(server_name, campaigns_being_removed_set)
-        # get deletion message and return it
-        deletion_message = self.get_campaign_deletion_message(invalid_campaign_for_removal)
-        return deletion_message
+        return self.check_campaign_removal(server_name, self.campaign_removal(valid_campaigns, server_name),
+                                           invalid_campaigns)
 
-    def individual_campaign_deletion(self, valid_campaign_name_for_removal, server_name):
-        # create empty set to hold info
-        campaign_names_being_removed_set = set()
+    def campaign_removal(self, campaigns_removing, server_name):
         # remove, from the database, each campaign that is in the valid for removal list
-        for campaign_names_being_removed in valid_campaign_name_for_removal:
-            database.cursor.execute(self.DELETE_CAMPAIGN_BY_NAME, server_name, campaign_names_being_removed)
-            sqlog.logger.info("%s\n Parameters: %s, %s", self.DELETE_CAMPAIGN_BY_NAME, server_name,
-                              campaign_names_being_removed)
-            # each time we do so, store away the name of the campaign we're removing
-            campaign_names_being_removed_set.add(campaign_names_being_removed)
-        # returns the set of campaigns we removed
-        return campaign_names_being_removed_set
+        try:
+            param = zip(itertools.repeat(server_name), campaigns_removing)
+            database.cursor.executemany(self.DELETE_CAMPAIGN_BY_NAME, param)
+            sqlog.logger.info("%s\n Parameters: %s, %s", self.DELETE_CAMPAIGN_BY_NAME, server_name, campaigns_removing)
+        except pyodbc.DatabaseError:
+            sqlog.logger.exception('Error Exception Occurred')
+        # then return the set of names we just removed which should only be the valid names
+        return campaigns_removing
 
-    def are_campaigns_deleted_properly(self, server_name, campaign_names_being_removed):
+    def check_campaign_removal(self, server_name, valid_campaigns,
+                               invalid_campaigns):
         # checks to see if the deleted set is still in the database
-        campaigns_still_there_check = set(self.get_campaigns(server_name)).intersection(campaign_names_being_removed)
-        if campaigns_still_there_check == set():
-            # an empty set means no campaigns set for deletion found in database
-            sqlog.logger.info('Committing...')
-            database.finalize()
-            sqlog.logger.info('Committed...')
+        if set(self.get_campaigns(server_name)).intersection(valid_campaigns):
+            test = set(self.get_campaigns(server_name)).intersection(valid_campaigns)
+            # if it isn't empty, stop and return a failure
+            return 'Campaigns not removed properly. See Ra.'
+        # an empty set means the campaigns were properly removed
+        sqlog.logger.info('Committing...')
+        database.finalize()
+        sqlog.logger.info('Committed...')
+        if invalid_campaigns:
+            return f"{', '.join([str(s) for s in valid_campaigns])} removed;" \
+                   f" {', '.join([str(s) for s in invalid_campaigns])} doesn't exist."
+        return f"{', '.join([str(s) for s in valid_campaigns])} removed."
 
-    def get_campaign_deletion_message(self, invalid_campaign_for_removal):
-        # get the campaigns as a string of campaigns
-        valid_string = ', '.join(str(s) for s in self.valid_campaign_for_removal)
-        invalid_string = ', '.join(str(s) for s in invalid_campaign_for_removal)
-        if self.valid_campaign_for_removal and invalid_campaign_for_removal:
-            # if both exist, give me both
-            deletion_message = f"{valid_string} removed; {invalid_string} non-existent."
-        elif self.valid_campaign_for_removal and not invalid_campaign_for_removal:
-            # if the input was only things to be removed
-            deletion_message = f"{valid_string} removed."
-        elif not self.valid_campaign_for_removal and invalid_campaign_for_removal:
-            # if the input was non-existent campaigns
-            deletion_message = f"{invalid_string} non-existent."
-        else:
-            deletion_message = f"'something went wrong: {valid_string} and {invalid_string}"
-        return deletion_message
-        # when do I want to show just invalid string
-
-    def delete_all_campaigns_in_server(self, server_name):
-        sqlog.logger.info('Deleting Campaign...')
+    def delete_server_campaigns(self, server_name):
+        sqlog.logger.info('Deleting Campaigns...')
         database.cursor.execute(self.DELETE_CAMPAIGNS, server_name)
-        sqlog.logger.info('Campaign Deleted...')
+        sqlog.logger.info('Campaigns Deleted...')
         database.finalize()
